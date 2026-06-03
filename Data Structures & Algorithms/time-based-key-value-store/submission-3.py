1ec0386618dda02ee9ae4095441fd61cc2a1bdc3
Rishabh Rathore <rishabhprathore@gmail.com>
2026-06-02 22:35:55 -0700

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,34 @@
+class TimeMap:
+
+    def __init__(self):
+        self.hashmap = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.hashmap:
+            self.hashmap[key] = []
+        self.hashmap[key].append((timestamp,value))
+        
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.hashmap:
+            return ""
+        values = self.hashmap[key]
+
+        left = 0
+        right = len(values)-1
+        res = ""
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            mid_t, mid_v = values[mid]
+
+            if mid_t <= timestamp:
+                res = mid_v
+
+                left = mid + 1
+            
+            else:
+                right = mid - 1
+        return res
+
+        
